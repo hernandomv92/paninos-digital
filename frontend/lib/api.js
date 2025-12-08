9ad6dab05686187651a7fc7db7e5a1ca307c456b
Nando <hernandomv@gmail.com>
2025-12-08 12:38:23 -0500

    fix: hardcode api url production

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // Lee la URL que configuramos en Easypanel, o usa localhost si falla
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://paninos-paninos-backend.ufiah8.easypanel.host',
     headers: {
         'Content-Type': 'application/json',
     },
